c7bd96cf5b90fea38f47992380243bd284800239
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-04-01 21:43:13 +0700

    intialize springboot

---
+++ src/main/java/id/ac/cs/advprog/hobbyshop/HobbyshopApplication.java
@@ -0,0 +1,13 @@
+package id.ac.cs.advprog.hobbyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbyshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HobbyshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/cs/advprog/hobbyshop/HobbyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.cs.advprog.hobbyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
